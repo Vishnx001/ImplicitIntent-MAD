ed3a49481a3fa0ae60a1c86530bdaa5ae094ab8f
Vishnx001 <134037148+Vishnx001@users.noreply.github.com>
2024-03-15 11:00:46 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/implicitintent/MainActivity.java
+++ app/src/main/java/com/example/implicitintent/MainActivity.java
@@ -1,7 +1,10 @@
+MainActivity.java
 package com.example.implicitintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent=new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("http:www.greeksforgreeks.org"));
+        startActivity(intent);
     }
 }
